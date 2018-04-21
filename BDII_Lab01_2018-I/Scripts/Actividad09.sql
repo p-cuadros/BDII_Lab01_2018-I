642201b205bcb70b11e4c15fe7b437e71c96ad96
Enrique <Enrique@DESKTOP-5D0H3DI>
2018-04-21 17:46:25 -0500

    Desarrollo actividad 4.9
    Desarrollo Actividad 4.9

---
+++ BDII_Lab01_2018-I/Scripts/Actividad09.sql
@@ -0,0 +1,58 @@
+﻿----------- SOLUCION 4.9
+-- 1
+-- la sentencia "deberá mostrar los Apellidos y Fecha de Contratación de todos los empleados del mismo departamento" es ambiguo
+-- suponiendo que existan 2 empleados con el mismo apellido pero de diferente departamento ¿que departamento prevalece?
+-- para el desarrollo se cambio apellido por id del empleado
+
+DECLARE @depid INT;
+DECLARE @empid INT;
+
+-- leyendo id de empleado
+SET @empid = 110
+-- obteniendo id de departamento de empleado
+SET @depid = (SELECT emp.department_id
+			  FROM employees as emp
+			  WHERE emp.employee_id=@empid);
+-- todos los empleados del mismo departamento excluyendo al empleado ingresado anteriormente
+SELECT emp.employee_id,
+	   emp.last_name,
+	   emp.hire_date,
+	   emp.department_id
+FROM employees AS emp
+WHERE emp.department_id=@depid
+AND emp.employee_id!=@empid;
+
+--/////////////////////////////////////////////////////////////////////////////////////////////
+-- 2
+-- Se considera "N° de empleado" como "id de empleado"
+-- Obteniendo promedio de salario
+DECLARE @prom DECIMAL(8,2); --Variable promedio
+SET @prom = (SELECT AVG(salary) FROM employees);
+
+-- Todos los empleados con sueldo superior al promedio
+SELECT emp.employee_id,
+       emp.last_name,
+	   emp.salary
+FROM employees AS emp
+WHERE emp.salary>@prom;
+
+--/////////////////////////////////////////////////////////////////////////////////////////////
+-- 3
+-- Se considera "N° de empleado" como "id de empleado"
+-- Obtener los id de departamentos de los empleados que contengan "u" en su apellido
+SELECT DISTINCT department_id
+			FROM employees
+			WHERE last_name LIKE '%u%';
+-- Obtener todos los empleados que laboren en alguno de los departamentos hallados anteriormente
+SELECT emp.employee_id,
+	   emp.last_name,
+	   emp.department_id
+	   FROM employees AS emp
+	JOIN (SELECT DISTINCT department_id
+		  FROM employees
+		  WHERE last_name LIKE '%u%') AS depid
+	ON emp.department_id=depid.department_id;
+
+--/////////////////////////////////////////////////////////////////////////////////////////////
+-- 4
+
