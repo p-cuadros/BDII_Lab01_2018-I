625fef311ef7ef387ea8b803aacee7ae8c3c23d7
jhony <AEPIS22@AEPIS22>
2018-04-21 13:19:26 -0500

    ACTIVIDAD09

--- BDII_Lab01_2018-I/Scripts/Actividad_09.sql
+++ BDII_Lab01_2018-I/Scripts/Actividad_09.sql
@@ -1,2 +1,6 @@
 ﻿
-
+SELECT  AVG( first_name) , AVG(last_name) ,
+AVG( employee_id) as 'in_empleado' ,  AVG(employees.salary	)as 'salario de empleados', SUM(employees.salary	)as 'SUMA salario'
+	FROM employees
+		
+	WHERE salary > AVG 	 order by [salario de empleados] desc ;	
