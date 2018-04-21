46bb6b9440189d25ad84c763a10417fb1071e37b
Luis <lanz.zav@gmail.com>
2018-04-21 13:20:24 -0500

    Zavala Venegas - Actividad08

--- BDII_Lab01_2018-I/Scripts/Actividad08.sql
+++ BDII_Lab01_2018-I/Scripts/Actividad08.sql
@@ -1,1 +1,11 @@
-﻿
+﻿//EJERCICIO 1
+
+SELECT location_id, street_address, city, state_province, country_name
+FROM locations
+NATURAL JOIN countries;
+
+
+//EJERCICIO 2
+SELECT last_name,departmet_id, department_date
+FROM locations
+JOIN countries;
