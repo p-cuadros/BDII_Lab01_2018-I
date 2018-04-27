b556f8d46ad5cbef142345f5a38bd88d53641a99
jhony <AEPIS22@AEPIS22>
2018-04-26 21:39:15 -0500

    terminado 09

--- BDII_Lab01_2018-I/Scripts/Actividad_09.sql
+++ BDII_Lab01_2018-I/Scripts/Actividad_09.sql
@@ -1,11 +1,84 @@
 ﻿
+-----1---------------------------------------------
 
 
-SELECT  AVG( first_name) , AVG(last_name) ,
-AVG( employee_id) as 'in_empleado' ,  AVG(employees.salary	)as 'salario de empleados', SUM(employees.salary	)as 'SUMA salario'
-	FROM employees
-		
-	WHERE salary > AVG 
 
-	 order by [salario de empleados] desc ;
+
+
+
+------2----------------------------------------------
+DECLARE @prom DECIMAL(8,2); 
+SET @prom = (SELECT AVG(salary) FROM employees);
+
+SELECT emp.employee_id,
+       emp.last_name,
+	   emp.salary
+FROM employees AS emp
+WHERE emp.salary>@prom;
+
+------3----------------------------------------
+
+SELECT DISTINCT department_id
+			FROM employees
+			WHERE last_name LIKE '%u%';
+
+SELECT emp.employee_id,
+	   emp.last_name,
+	   emp.department_id
+	   FROM employees AS emp
+	JOIN (SELECT DISTINCT department_id
+		  FROM employees
+		  WHERE last_name LIKE '%u%') AS depid
+	ON emp.department_id=depid.department_id;
+
+------------4-----------------------------
+
+
+SELECT emp.last_name,
+	   emp.department_id,
+	   dep.location_id
+	   FROM employees as emp
+JOIN departments as dep
+	ON emp.department_id=dep.department_id
+	WHERE dep.location_id=1700;
+
+------5----------------------------------------
+
+DECLARE @locid INT;
+SET @locid = 1700;
+SELECT emp.last_name,
+	   emp.department_id,
+	   dep.location_id
+	   FROM employees as emp
+JOIN departments as dep
+	ON emp.department_id=dep.department_id
+	WHERE dep.location_id=@locid;
+
+----------6----------------------------------------------
+select * from employees where department_id=90;
+select * from jobs;
+select * from departments where department_name='executive';
+
+
+SELECT emp.department_id,
+       emp.last_name,
+	   jobs.job_title
+	   FROM employees AS emp
+JOIN jobs
+ON emp.job_id=jobs.job_id;
+
+
+SELECT empnomjob.department_id,
+	   empnomjob.last_name,
+	   empnomjob.job_title
+	   FROM departments
+JOIN (SELECT emp.department_id,
+			 emp.last_name,
+			 jobs.job_title
+			 FROM employees AS emp
+	  JOIN jobs
+	  ON emp.job_id=jobs.job_id) AS empnomjob
+ON empnomjob.department_id=departments.department_id
+WHERE department_name='executive'
+
 	
