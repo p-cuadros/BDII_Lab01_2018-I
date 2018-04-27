5c8571fdc7d437863c24194107a0dad455e58015
ADOCENTE <ADOCENTE@ADOCENTE>
2018-04-26 20:46:34 -0500

    Corrección del archivo 09

--- BDII_Lab01_2018-I/Scripts/Actividad_09.sql
+++ BDII_Lab01_2018-I/Scripts/Actividad_09.sql
@@ -1,6 +1,11 @@
 ﻿
-SELECT  AVG( first_name) , AVG(last_name) ,
+
+
+SELECT  AVG( first_name) , AVG(last_name) ,
 AVG( employee_id) as 'in_empleado' ,  AVG(employees.salary	)as 'salario de empleados', SUM(employees.salary	)as 'SUMA salario'
 	FROM employees
 		
-	WHERE salary > AVG 	 order by [salario de empleados] desc ;	
+	WHERE salary > AVG 
+
+	 order by [salario de empleados] desc ;
+	
